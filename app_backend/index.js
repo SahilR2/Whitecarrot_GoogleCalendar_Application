8ab575f3cef1491a86a76b0b83f98e82fac056ee
Syed Shabiul Husain <139258098+SahilR2@users.noreply.github.com>
2025-01-20 01:29:45 +0530

    Update index.js

--- app_backend/index.js
+++ app_backend/index.js
@@ -33,7 +33,7 @@ passport.use(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://whitecarrot-googlecalendar-application-1.onrender.com//auth/google/callback",
+        callbackURL: "https://whitecarrot-googlecalendar-application-1.onrender.com/auth/google/callback",
       },
       (accessToken, refreshToken, profile, done) => {
         
